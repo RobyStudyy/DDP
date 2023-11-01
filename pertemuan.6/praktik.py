7f0c1e6a038fe76692e9a2b66e6ab02c0eeece87
RobyStudyy <robymulia20@gmail.com>
2023-11-01 10:34:39 +0700

    pertemuan6

---
+++ pertemuan.6/praktik.py
@@ -0,0 +1,21 @@
+a=100
+while a>=1:
+    print(a)
+    a-=1
+
+    print("=================")
+
+    for i in range(50,0,-5):
+        print(i)
+
+for kamu in range(3):
+    print("ical")
+
+for i in range(1,11):
+    for j in range(1,11):
+        k=i*j
+        print(k,end='')
+    print()
+
+    print()
+
